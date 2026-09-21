9b2743841d9ec5ea780ab446e13b16970ede5fe3
Muntasir Shawon <muntasir.shawon78@gmail.com>
2026-09-21 21:13:49 +0600

    fix(map): Replace Carto with Esri World Dark Gray base and reference layers (zero API key required)

--- frontend/src/components/RiskMap.tsx
+++ frontend/src/components/RiskMap.tsx
@@ -256,8 +256,14 @@ export const RiskMap: React.FC = () => {
             style={{ height: '100%', width: '100%', backgroundColor: '#0f172a' }}
           >
             <TileLayer
-              attribution='&copy; <a href="https://carto.com/">CARTO</a>'
-              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+              attribution='&copy; <a href="https://www.esri.com/">Esri</a>'
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+              maxZoom={16}
+            />
+            <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Reference/MapServer/tile/{z}/{y}/{x}"
+              maxZoom={16}
+              opacity={0.8}
             />
 
             {/* Hotspots Layer */}
--- frontend/src/components/RouteFinder.tsx
+++ frontend/src/components/RouteFinder.tsx
@@ -371,8 +371,14 @@ export const RouteFinder: React.FC = () => {
               zoomControl={true}
             >
               <TileLayer
-                attribution='&copy; <a href="https://carto.com/">CARTO</a>'
-                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                attribution='&copy; <a href="https://www.esri.com/">Esri</a>'
+                url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+                maxZoom={16}
+              />
+              <TileLayer
+                url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Reference/MapServer/tile/{z}/{y}/{x}"
+                maxZoom={16}
+                opacity={0.8}
               />
 
               <MapAutoBounds coords={activeRoute.path_coordinates} />
